2034f33abd51eea33ef8bd8f5854c66a3bf115f3
Austin Horn <91858347+austinmhorn@users.noreply.github.com>
2023-06-29 14:04:48 -0500

    Add brew_fetch_cmake script

---
+++ brew_fetch_cmake/install.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+# Reset
+Reset='\033[0m'           # Text Reset
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# Check for bash
+if [ -v "${BASH_VERSION:-}" ]
+then
+    echo $Red">>"$Reset" Bash is required to interpret this script."
+else
+    echo $Yellow">>"$Reset" Found bash"
+    which bash
+fi
+
+ring_bell() {
+  # Use the shell's audible bell.
+  if [[ -t 1 ]]
+  then
+    printf "\a"
+  fi
+}
+
+# Determine OS
+OS="$OSTYPE"
+
+# Determine Architecture
+ARCH=$(uname -m)
+
+# OS Specific Operations
+echo $Yellow">>"$Reset" Detecting Operating System"
+case "$OS" in
+  darwin*)
+    echo "OSX"
+    if [[ ! $(command which -s xcode-select) == 0 ]]; then
+      echo "** Detecting Xcode Command Line Tools - done"
+      which xcode-select
+    else
+      echo "** Installing Xcode Command Line Tools"
+      xcode-select --install
+    fi ;;
+  linux*)
+    echo "Linux"
+    if [[ ! $(command which -s xcode-select) == 0 ]]; then
+      echo "** Detecting Xcode Command Line Tools - done"
+      which xcode-select
+    else
+      echo "** Installing Xcode Command Line Tools"
+      xcode-select --install
+    fi
+    ;;
+  bsd*)
+    echo "FreeBSD" ;;
+  solaris*)
+    echo "Solaris" ;;
+  msys*)
+    echo "Windows Bash/msysGit" ;;
+  cygwin*)
+    echo "Windows Cygwin" ;;
+  *)
+    echo -e "${RED}ERROR:${WHITE}"
+    echo "Unknown OS: $OSTYPE"
+    echo "Software is not compatible with architecture"
+    exit 1 ;;
+esac
+
+# Determine architecture
+if [[ "$ARCH" == x86_64* ]]; then
+    echo "X64"
+elif [[ "$ARCH" == i*86 ]]; then
+    echo "X32"
+elif  [[ "$ARCH" == arm* ]]; then
+    echo "ARM"
+fi
+
+# Check for Homebrew
+echo "Detecting Homebrew"
+if [[ $(command -v brew) == "" ]]; then
+    echo $Yellow">>"$Reset" Installing Homebrew"
+    ring_bell
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo $Yellow">>"$Reset" Found brew"
+    which brew
+fi
+echo $Yellow">>"$Reset" Detecting Hoembrew - done"
+
+# Check for cmake
+echo $Yellow">>"$Reset" Detecting cmake"
+if [[ $(command -v cmake) == "" ]]; then
+    echo $Yellow">>"$Reset" Installing cmake"
+    ring_bell
+    brew install cmake
+else
+    echo $Yellow">>"$Reset" Found cmake"
+    which cmake
+fi
+echo $Yellow">>"$Reset" Detecting cmake - done"
