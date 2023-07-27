7b10dd79a6b5ab11fb9b27d51d75596aeeb90ea5
Austin Horn <91858347+austinmhorn@users.noreply.github.com>
2023-07-26 20:25:38 -0500

    Add files via upload

---
+++ find-homebrew.sh
@@ -0,0 +1,10 @@
+#!usr/bin/bash
+
+# Check for Homebrew
+if [[ $(command -v brew) == "" ]]; then
+    echo "Installing Homebrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Updating Homebrew"
+    brew update
+fi
