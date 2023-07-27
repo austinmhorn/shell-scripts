b53c9fe96d2f28140903c4737123b733f45a6d08
Austin Horn <91858347+austinmhorn@users.noreply.github.com>
2023-07-26 20:22:05 -0500

    Update install.sh

--- brew_fetch_cmake/install.sh
+++ brew_fetch_cmake/install.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-# Reset
-Reset='\033[0m'           # Text Reset
-Black='\033[0;30m'        # Black
-Red='\033[0;31m'          # Red
-Green='\033[0;32m'        # Green
-Yellow='\033[0;33m'       # Yellow
-Blue='\033[0;34m'         # Blue
-Purple='\033[0;35m'       # Purple
-Cyan='\033[0;36m'         # Cyan
-White='\033[0;37m'        # White
+source decorations.sh
 
 # Check for bash
 if [ -v "${BASH_VERSION:-}" ]
