edcafafb7a2d6126369c16e5eff066c5c7156abe
Austin Horn <91858347+austinmhorn@users.noreply.github.com>
2024-12-05 15:43:15 -0600

    Delete sfml directory

--- sfml/include/Core/Sansation.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef Sansation_hpp
-#define Sansation_hpp
-
-#include <SFML/Graphics/Text.hpp>
-
-sf::Font load_sansation();
-
-#endif /* Sansation_hpp */
--- sfml/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Core/Application.hpp"
-
-int main(int arc, const char **argv) 
-{
-    auto app = Application{};
-    app.run();
-
-    return 0;
-}
